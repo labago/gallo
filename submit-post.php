289ab677cdec605f85e8299da5c80ddc15c6306d
Jonathan Lane <jlane09sjp@gmail.com>
2013-10-08 18:07:17 -0400

    fixed blog submition error

--- submit-post.php
+++ submit-post.php
@@ -1,15 +1,34 @@
 <?php
 include("functions.php");
 
-$author =  htmlentities($_POST['author']);
-$title = $_POST['title'];
-$body =  htmlentities($_POST['body']);
+$author =  mysql_real_escape_string(htmlentities($_POST['author']));
+$title = mysql_real_escape_string($_POST['title']);
+$body =  mysql_real_escape_string(htmlentities($_POST['body']));
 $category = $_POST['category'];
-$abstract = htmlentities($_POST['abstract']);
+$abstract = mysql_real_escape_string(htmlentities($_POST['abstract']));
 $pic = $_POST['pic'];
-$tags =  htmlentities($_POST['tags']);
+$tags =  mysql_real_escape_string(htmlentities($_POST['tags']));
 
 if(add_blog_post($author, $title, $body, "", $abstract, $pic, $category, $tags = ""))
 	echo "Post successfully added!";
 
+echo "<br>";
+echo "<br>";
+echo "Author: ".$author;
+echo "<br>";
+echo "<br>";
+echo "Title: ".$title;
+echo "<br>";
+echo "<br>";
+echo "Body: <br><br>".$body;
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "Category: ".$category;
+echo "<br>";
+echo "<br>";
+echo "Abstract: ".$abstract;
+echo "<br>";
+echo "<br>";
+echo "Tags: ".$tags;
 ?>
